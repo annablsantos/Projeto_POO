83ab443a84c620d1ef9fb7e0b3278b85810e2f70
Anna Beatriz <annabea608@gmail.com>
2024-11-26 16:36:22 -0300

    Primeiro commit - Configuração inicial do projeto

---
+++ src/interfaces/Heroi.java
@@ -0,0 +1,6 @@
+package interfaces;
+
+public interface Heroi{
+    String getNome();
+    int getVida();
+}
---
+++ src/model/Poder.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Poder {
+    private String nome;
+    private int dano;
+    private int consumoDeEnergia;
+}
