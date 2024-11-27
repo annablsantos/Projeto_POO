0307e655a67ac56c3059e13e06cb0119e9cab8ea
azineu <azineu.wng@2outlook.com>
2024-11-27 19:46:11 -0300

    Adicionando models de heróis.

--- src/models/HeroiElemental.java
+++ src/models/HeroiElemental.java
@@ -1,4 +1,33 @@
 package models;
 
-public class HeroiElemental {
+import interfaces.Heroi;
+
+public class HeroiElemental extends Personagem implements Heroi {
+    private Poder poderElemental;
+
+    public HeroiElemental(String nome) {
+        super(nome);
+    }
+
+    public void aprenderPoderEspecial(String nomePoder, Poder poder) {
+        this.poderElemental = poder;
+    }
+
+    @Override
+    public boolean temSuperPoder(String nomePoder) {
+        return poderElemental != null && poderElemental.getNome().equals(nomePoder);
+    }
+
+    @Override
+    public boolean desviar() {
+        return true;
+    }
+
+    @Override
+    public String usarSuperPoder(Poder superPoder) {
+        if (temSuperPoder(superPoder.getNome())) {
+            return "O herói " + getNome() + " usou " + superPoder.getNome() + " para atacar!";
+        }
+        return null;
+    }
 }
--- src/models/HeroiFisico.java
+++ src/models/HeroiFisico.java
@@ -1,4 +1,33 @@
 package models;
 
-public class HeroiFisico {
+import interfaces.Heroi;
+
+public class HeroiFisico extends Personagem implements Heroi {
+    private Poder poderFisico;
+
+    public HeroiFisico(String nome) {
+        super(nome);
+    }
+
+    public void aprenderPoderEspecial(String nomePoder, Poder poder) {
+        this.poderFisico = poder;
+    }
+
+    @Override
+    public boolean temSuperPoder(String nomePoder) {
+        return poderFisico != null && poderFisico.getNome().equals(nomePoder);
+    }
+
+    @Override
+    public boolean desviar() {
+        return true;
+    }
+
+    @Override
+    public String usarSuperPoder(Poder superPoder) {
+        if (temSuperPoder(superPoder.getNome())) {
+            return "O herói " + getNome() + " usou " + superPoder.getNome() + " para atacar!";
+        }
+        return null;
+    }
 }
